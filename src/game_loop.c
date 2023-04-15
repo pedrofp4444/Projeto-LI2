ada7eb3b5b33e57e1c8dba87ef6cae6d65927866
Pedro Pereira <109802203+pedrofp4444@users.noreply.github.com>
2023-04-15 20:42:49 +0000

    Revert "Render Map (#6)" (#7)
    This reverts commit d1e147f490141099fa0e363d74132a11b9f831c1.

--- src/game_loop.c
+++ src/game_loop.c
@@ -34,29 +34,21 @@
 int game_loop_ignore_signal(int signum) {
 	struct sigaction act = {
 		.sa_handler = SIG_IGN,
-		.sa_flags = SA_NODEFER,
+		.sa_flags   = SA_NODEFER,
 	};
 
 	return sigaction(signum, &act, NULL);
 }
 
 int game_loop_init_ncurses(void) {
-	if (initscr() == NULL)
-		return 1;
-	if (start_color() == ERR)
-		return 1; /* Enable color support */
-	if (cbreak() == ERR)
-		return 1; /* Disable line buffering and control characters */
-	if (noecho() == ERR)
-		return 1; /* Don't show input on the terminal */
-	if (nodelay(stdscr, 1) == ERR)
-		return 1; /* Make getch return ERR if no input is available */
-	if (nonl() == ERR)
-		return 1; /* Transform new lines intro carriage returns */
-	if (keypad(stdscr, 1) == ERR)
-		return 1; /* Let ncurses parse escape sequences */
-	if (curs_set(0) == ERR)
-		return 1; /* Hide the cursor */
+	if (initscr()           == NULL) return 1;
+	if (start_color()        == ERR) return 1; /* Enable color support */
+	if (cbreak()             == ERR) return 1; /* Disable line buffering and control characters */
+	if (noecho()             == ERR) return 1; /* Don't show input on the terminal */
+	if (nodelay(stdscr, 1)   == ERR) return 1; /* Make getch return ERR if no input is available */
+	if (nonl()               == ERR) return 1; /* Transform new lines intro carriage returns */
+	if (keypad(stdscr, 1)    == ERR) return 1; /* Let ncurses parse escape sequences */
+	if (curs_set(0)          == ERR) return 1; /* Hide the cursor */
 
 	/* Limit of 10ms for ncurses to give up on finding characters for escape sequences */
 	ESCDELAY = 10;
@@ -84,89 +76,24 @@ double timespec_dif(struct timespec start, struct timespec end) {
 }
 
 /**
- * @brief Internal game loop function for keeping the terminal window size up to date./**
- * @file map.h
- * @brief Game Map
- */
-
-/*
- *   Copyright 2023 Hélder Gomes, Humberto Gomes, Mariana Rocha, Pedro Pereira
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
-#ifndef MAP_H
-#define MAP_H
-
-typedef enum {
-    TILE_EMPTY,
-    TILE_WALL,
-} tile_type;
-
-typedef struct {
-    tile_type type;
-} tile;
-
-typedef struct {
-    char chr;
-} tile_type_render_info;
-
-tile_type_render_info tile_get_render_info(tile_type t);
-
-/**
- * @var map::data
- *   To access coordinate (x, y), use `data[y * width + x]`
- */
-typedef struct {
-    unsigned width, height;
-    tile *data;
-} map;
-
-/**
- * @brief Creates (and allocates memory for) a map
- * @returns On error, a map with a `NULL` `data` pointer.
- *
- * Map data will be memory trash.
- */
-map map_allocate(unsigned width, unsigned height);
-
-void map_render(map map,
-                unsigned map_top, unsigned map_left,
-                unsigned term_top, unsigned term_left,
-                unsigned height, unsigned width);
-
-#endif
-
+ * @brief Internal game loop function for keeping the terminal window size up to date.
  *
  * @returns The return value of the resize callback in case of a loop exit request. Otherwise,
  *          ::GAME_LOOP_CALLBACK_RETURN_SUCCESS is returned.
  */
 game_loop_callback_return_value game_loop_window_size(void *state, int *width, int *height,
-													  game_loop_resize_callback onresize) {           
+                                                      game_loop_resize_callback onresize) {
 	int nwidth, nheight;
 	getmaxyx(stdscr, nheight, nwidth);
 
-	if (!(nwidth == *width && nheight == *height)) { 
-		/* Window size changed */
+	if (!(nwidth == *width && nheight == *height)) { /* Window size changed */
 
 		if (onresize) {
 			int ret = onresize(state, nwidth, nheight);
-			if (ret != GAME_LOOP_CALLBACK_RETURN_SUCCESS)
-				return ret;
+			if (ret != GAME_LOOP_CALLBACK_RETURN_SUCCESS) return ret;
 		}
 
-		*width = nwidth;
-		*height = nheight;
+		*width = nwidth; *height = nheight;
 	}
 
 	return GAME_LOOP_CALLBACK_RETURN_SUCCESS;
@@ -178,15 +105,14 @@ game_loop_callback_return_value game_loop_window_size(void *state, int *width, i
  * @returns The return value of the input callback in case of a loop exit request. Otherwise,
  *          ::GAME_LOOP_CALLBACK_RETURN_SUCCESS is returned.
  */
-game_loop_callback_return_value game_loop_handle_input(void *state, game_loop_input_callback oninput) {
+game_loop_callback_return_value game_loop_handle_input
+	(void *state, game_loop_input_callback oninput) {
 
-	if (oninput) {
-		/* Skip reading input if no input callback is defined */
+	if (oninput) { /* Skip reading input if no input callback is defined */
 		int c = getch();
 		while (c != ERR) {
 			int ret = oninput(state, c);
-			if (ret != GAME_LOOP_CALLBACK_RETURN_SUCCESS)
-				return ret;
+			if (ret != GAME_LOOP_CALLBACK_RETURN_SUCCESS) return ret;
 
 			c = getch();
 		}
@@ -202,8 +128,7 @@ game_loop_callback_return_value game_loop_handle_input(void *state, game_loop_in
 int game_loop_keep_fps(struct timespec frame_start, double frame_time) {
 	/* Calculate time since the beginning of this frame */
 	struct timespec frame_end;
-	if (clock_gettime(CLOCK_MONOTONIC, &frame_end))
-		return 1;
+	if (clock_gettime(CLOCK_MONOTONIC, &frame_end)) return 1;
 	double delta = timespec_dif(frame_start, frame_end);
 
 	/* Sleep for enough time to target the framerate */
@@ -212,12 +137,12 @@ int game_loop_keep_fps(struct timespec frame_start, double frame_time) {
 		sleep_time = (frame_time - delta) * 1e9; /* 1e9: s -> ns */
 
 	/*
-	 * NOTES: - tv_nsec is lower than 1e9 because frame_time can't be larger than a second
-	 *
-	 *        - No error handling for nanosleep (errors out on external signals), as it's not a
-	 *          crucial function to keep the game running.
-	 */
-	struct timespec sleep = {.tv_sec = 0, .tv_nsec = (long)sleep_time};
+	* NOTES: - tv_nsec is lower than 1e9 because frame_time can't be larger than a second
+	*
+	*        - No error handling for nanosleep (errors out on external signals), as it's not a
+	*          crucial function to keep the game running.
+	*/
+	struct timespec sleep = { .tv_sec = 0, .tv_nsec = (long) sleep_time };
 	nanosleep(&sleep, NULL);
 
 	return 0;
@@ -227,30 +152,26 @@ int game_loop_keep_fps(struct timespec frame_start, double frame_time) {
  * @brief Helper macro for ::game_loop_run that, based on the return value of a callback,
  *        determines whether to continue looping and the game loop's return value (if needed).
  */
-#define game_loop_return(val)                              \
-	{                                                      \
-		if (val != GAME_LOOP_CALLBACK_RETURN_SUCCESS)      \
-			return val == GAME_LOOP_CALLBACK_RETURN_ERROR; \
-	}
+#define game_loop_return(val) { \
+	if (val != GAME_LOOP_CALLBACK_RETURN_SUCCESS) \
+		return val == GAME_LOOP_CALLBACK_RETURN_ERROR; \
+}
 
 int game_loop_run(void *state, game_loop_callbacks callbacks, unsigned int fps) {
 	int width = -1, height = -1;
 
 	struct timespec last_frame_instant;
-	if (clock_gettime(CLOCK_MONOTONIC, &last_frame_instant))
-		return 1;
+	if (clock_gettime(CLOCK_MONOTONIC, &last_frame_instant)) return 1;
 
 	double frame_time = 0;
 	if (fps)
 		frame_time = 1.0 / fps;
-	if (fps == 1)
-		frame_time = 0.9999; /* Edge case when converting to timespec */
+	if (fps == 1) frame_time = 0.9999; /* Edge case when converting to timespec */
 
 	while (1) {
 		/* Calculate frame time (since the beginning of the last frame) */
 		struct timespec frame_instant;
-		if (clock_gettime(CLOCK_MONOTONIC, &frame_instant))
-			return 1;
+		if (clock_gettime(CLOCK_MONOTONIC, &frame_instant)) return 1;
 		double delta = timespec_dif(last_frame_instant, frame_instant);
 		last_frame_instant = frame_instant;
 
@@ -261,16 +182,13 @@ int game_loop_run(void *state, game_loop_callbacks callbacks, unsigned int fps) 
 		ret = game_loop_handle_input(state, callbacks.oninput);
 		game_loop_return(ret);
 
-		if (callbacks.onupdate)
-			ret = callbacks.onupdate(state, delta);
+		if (callbacks.onupdate) ret = callbacks.onupdate(state, delta);
 		game_loop_return(ret);
 
-		if (callbacks.onrender)
-			ret = callbacks.onrender(state, width, height);
+		if (callbacks.onrender) ret = callbacks.onrender(state, width, height);
 		game_loop_return(ret);
 
-		if (game_loop_keep_fps(frame_instant, frame_time))
-			return 1;
+		if (game_loop_keep_fps(frame_instant, frame_time)) return 1;
 	}
 
 	return 0;
@@ -279,3 +197,4 @@ int game_loop_run(void *state, game_loop_callbacks callbacks, unsigned int fps) 
 int game_loop_terminate_ncurses(void) {
 	return endwin() == ERR; /* Restore previous terminal mode */
 }
+
--- src/main.c
+++ src/main.c
@@ -20,40 +20,36 @@
  */
 
 #include <game_loop.h>
-#include <map.h>
-
 #include <ncurses.h>
-#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
 	double fps;
-	map map;
+
 	int offsetx, offsety;
 } game_state;
 
 /** @brief Handle user input (move text with arrows, exit program on escape) */
 game_loop_callback_return_value oninput(void *s, int key) {
-	game_state *state = (game_state *)s;
+	game_state *state = (game_state *) s;
 
-	switch (key)
-	{
-	case '\x1b':
-		return GAME_LOOP_CALLBACK_RETURN_BREAK; /* Exit game on escape */
+	switch (key) {
+		case '\x1b':
+			return GAME_LOOP_CALLBACK_RETURN_BREAK; /* Exit game on escape */
 
-	case KEY_UP:
-		state->offsety--;
-		break;
-	case KEY_DOWN:
-		state->offsety++;
-		break;
+		case KEY_UP:
+			state->offsety--;
+			break;
+		case KEY_DOWN:
+			state->offsety++;
+			break;
 
-	case KEY_LEFT:
-		state->offsetx--;
-		break;
-	case KEY_RIGHT:
-		state->offsetx++;
-		break;
+		case KEY_LEFT:
+			state->offsetx--;
+			break;
+		case KEY_RIGHT:
+			state->offsetx++;
+			break;
 	}
 
 	return GAME_LOOP_CALLBACK_RETURN_SUCCESS;
@@ -61,26 +57,73 @@ game_loop_callback_return_value oninput(void *s, int key) {
 
 /** @brief Update the game state (keep count of the FPS) */
 game_loop_callback_return_value onupdate(void *s, double elapsed) {
-	game_state *state = (game_state *)s;
+	game_state *state = (game_state *) s;
 	state->fps = 1.0 / elapsed;
 	return GAME_LOOP_CALLBACK_RETURN_SUCCESS;
 }
 
+/** @brief Print @p str in a rainbow color pattern */
+void print_rainbow(const char *str) {
+	int color, state = 0;
+	while (*str) {
+		switch (state) {
+			case 0:
+				color = COLOR_MAGENTA;
+				break;
+			case 1:
+				color = COLOR_BLUE;
+				break;
+			case 2:
+				color = COLOR_GREEN;
+				break;
+			case 3:
+				color = COLOR_YELLOW;
+				break;
+			/* Note that orange is not in the standard colors */
+			case 4:
+				color = COLOR_RED;
+				break;
+			default:
+				break;
+		}
+
+		attron(COLOR_PAIR(color));
+		addch(*str);
+		attroff(COLOR_PAIR(color));
+
+
+		state = (state + 1) % 5;
+		str++;
+	}
+}
+
 /** @brief Render the test game */
 game_loop_callback_return_value onrender(void *s, int width, int height) {
-	game_state *state = (game_state *)s;
-
-	(void)width;
-	(void)height;
+	game_state *state = (game_state *) s;
 
 	erase();
 
-	map_render(state->map,
-			   state->offsety, state->offsetx, 5, 2, 20, 20);
-
 	/* Print the FPS on the top right corner */
 	move(0, 0);
-	printw("FPS: %d", (int)state->fps);
+	printw("FPS: %d", (int) state->fps);
+
+	/* Print the window size and color info in the center of the window (plus input offset) */
+	char str[128];
+	int len = sprintf(str, "%d x %d", width, height);
+
+	move(height / 2 + state->offsety, (width - len) / 2 + state->offsetx);
+	printw("%s", str);
+
+	len = sprintf(str, "COLORS: %d, COLOR_PAIRS: %d", COLORS, COLOR_PAIRS);
+
+	move(height / 2 + state->offsety + 1, (width - len) / 2 + state->offsetx);
+	printw("%s", str);
+
+	/* Print a message on the bottom of the screen (with color) */
+	strcpy(str, "Fight for LGBTQ+ rights");
+	len = strlen(str);
+	move(height - 1, (width - len) / 2);
+	print_rainbow(str);
 
 	refresh();
 
@@ -98,25 +141,13 @@ int main(void) {
 	}
 
 	game_loop_callbacks callbacks = {
-		.oninput = oninput,
+		.oninput  = oninput,
 		.onupdate = onupdate,
 		.onrender = onrender,
-		.onresize = NULL};
+		.onresize = NULL
+	};
 
-	game_state state = {.fps = 0.0, .map = map_allocate(1024, 1024)};
-	if (state.map.data == NULL) {
-		/* Don't handle errors. Just try to return to a canonical terminal mode */
-		game_loop_terminate_ncurses();
-		puts("Could not initialize ncurses!");
-		return 1;
-	}
-
-	int arrcount = state.map.width * state.map.height;
-	memset(state.map.data, 0, arrcount * sizeof(tile));
-	tile wall = {.type = TILE_WALL};
-	tile empty = {.type = TILE_EMPTY};
-	for (int i = 0; i < arrcount; ++i)
-		state.map.data[i] = ((rand() % 2) == 0) ? wall : empty;
+	game_state state = { .fps = 0.0 };
 
 	err = game_loop_run(&state, callbacks, 60);
 	if (err) {
@@ -129,3 +160,4 @@ int main(void) {
 	err = game_loop_terminate_ncurses();
 	return err;
 }
+
