4974b189e117c58e370301043ee9fd9c4cf9f7a5
Mariana <108686064+Mariana-rc01@users.noreply.github.com>
2023-04-16 09:50:45 +0100

    Update include/map.h
    Co-authored-by: Voidbert <50591320+voidbert@users.noreply.github.com>

--- include/map.h
+++ include/map.h
@@ -69,7 +69,12 @@ tile_type_render_info tile_get_render_info(tile_type t);
  * @brief A pointer for the first element of the tile data array.
  * @var map::data
  * 
- * The `data` variable is a pointer to the first element of the tile data array,
+ * @var map::width
+ *   Width of the map in the tiles
+ * @var map::height
+ *   Height of the map in the tiles
+ * @var map::data
+ *   The `data` variable is a pointer to the first element of the tile data array,
  * which is dynamically alocated and stores the tile data for the map.
  *   To access coordinate (x, y), use the expression `data[y * width + x]`.
  *   The array size is `with*height` and each element is of the type `tile`.
